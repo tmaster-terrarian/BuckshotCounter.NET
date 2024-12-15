de8eb9f7946c233ad99cefc8b14f8b04b9aa49bc
bscit <54488066+tmaster-terrarian@users.noreply.github.com>
2024-12-14 18:29:11 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using Photino.NET;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace BuckshotCounter.NET;
+
+//NOTE: To hide the console window, go to the project properties and change the Output Type to Windows Application.
+// Or edit the .csproj file and change the <OutputType> tag from "WinExe" to "Exe".
+internal class Program
+{
+    [STAThread]
+    static void Main(string[] args)
+    {
+        // Window title declared here for visibility
+        string windowTitle = "Photino for .NET Demo App";
+
+        // Creating a new PhotinoWindow instance with the fluent API
+        var window = new PhotinoWindow()
+            .SetTitle(windowTitle)
+            // Resize to a percentage of the main monitor work area
+            .SetUseOsDefaultSize(false)
+            .SetSize(new Size(1024, 800))
+            // Center window in the middle of the screen
+            .Center()
+            // Users can resize windows by default.
+            // Let's make this one fixed instead.
+            .SetResizable(false)
+            .SetDevToolsEnabled(false)
+            .RegisterCustomSchemeHandler("app", (object sender, string scheme, string url, out string contentType) =>
+            {
+                contentType = "text/javascript";
+                return new MemoryStream(Encoding.UTF8.GetBytes(@"
+                        (() =>{
+                            window.setTimeout(() => {
+                                alert(`🎉 Dynamically inserted JavaScript.`);
+                            }, 1000);
+                        })();
+                    "));
+            })
+            // Most event handlers can be registered after the
+            // PhotinoWindow was instantiated by calling a registration 
+            // method like the following RegisterWebMessageReceivedHandler.
+            // This could be added in the PhotinoWindowOptions if preferred.
+            .RegisterWebMessageReceivedHandler((object sender, string message) =>
+            {
+                OpenUrl(message);
+            })
+            .Load("wwwroot/index.html"); // Can be used with relative path strings or "new URI()" instance to load a website.
+
+        window.WaitForClose(); // Starts the application event loop
+    }
+
+    private static void OpenUrl(string url)
+    {
+        try
+        {
+            Process.Start(url);
+        }
+        catch
+        {
+            // hack because of this: https://github.com/dotnet/corefx/issues/10361
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                url = url.Replace("&", "^&");
+                Process.Start(new ProcessStartInfo(url) {
+                    UseShellExecute = true
+                });
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                Process.Start("xdg-open", url);
+            }
+            else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                Process.Start("open", url);
+            }
+            else
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# BuckshotCounter.NET
+ fully-local .NET port of buckshot-counter
